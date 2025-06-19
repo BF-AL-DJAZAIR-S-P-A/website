5766bb28d72e5ee34d04dc1c1c6f9b3d45589f13
Mehdi Boumediene <elm3hdi@gmail.com>
2025-06-19 14:20:27 +0100

    ck

--- public/ck/main.js
+++ public/ck/main.js
@@ -3,7 +3,7 @@
  * https://ckeditor.com/ckeditor-5/builder/?redirect=portal#installation/NoNgNARATAdCMEYKQQBgCwFYDMICcuA7HgBzaEUl54gJTZRQkLqHbpRsbaYKGaEQyCADMATslRhgCMFPlywCALqQq1AEYbUEZUA=
  */
 
-import {
+const {
 	ClassicEditor,
 	Alignment,
 	Autosave,
@@ -40,14 +40,13 @@ import {
 	TableProperties,
 	TableToolbar,
 	Underline
-} from 'ckeditor5';
-
-import translations from 'ckeditor5/translations/fr.js';
+} = window.CKEDITOR;
 
 /**
- * Create a free account with a trial: https://portal.ckeditor.com/checkout?plan=free
+ * This is a 24-hour evaluation key. Create a free account to use CDN: https://portal.ckeditor.com/checkout?plan=free
  */
-const LICENSE_KEY = 'GPL'; // or <YOUR_LICENSE_KEY>.
+const LICENSE_KEY =
+	'eyJhbGciOiJFUzI1NiJ9.eyJleHAiOjE3ODE5MTM1OTksImp0aSI6IjgyYmMyYzA1LTMwZDctNDU4OC05ZGY3LWMzZTBiZGQ2MmE1MCIsInVzYWdlRW5kcG9pbnQiOiJodHRwczovL3Byb3h5LWV2ZW50LmNrZWRpdG9yLmNvbSIsImRpc3RyaWJ1dGlvbkNoYW5uZWwiOlsiY2xvdWQiLCJkcnVwYWwiXSwiZmVhdHVyZXMiOlsiRFJVUCIsIkUyUCIsIkUyVyJdLCJ2YyI6Ijk1YjZjYzliIn0.d26bcvovY27Rtd_WSFzdWFvo7eGxlQOtf2Y-DSpS_bdEu-4fRHDzmrCACMX4Lb36FddK9U9SWTmiOQuHdBZKFw';
 
 const editorConfig = {
 	toolbar: {
@@ -187,8 +186,7 @@ const editorConfig = {
 			}
 		]
 	},
-	initialData:
-		'<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n\tYou\'ve successfully created a CKEditor 5 project. This powerful text editor\n\twill enhance your application, enabling rich text editing capabilities that\n\tare customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n\t<li>\n\t\t<strong>Integrate into your app</strong>: time to bring the editing into\n\t\tyour application. Take the code you created and add to your application.\n\t</li>\n\t<li>\n\t\t<strong>Explore features:</strong> Experiment with different plugins and\n\t\ttoolbar options to discover what works best for your needs.\n\t</li>\n\t<li>\n\t\t<strong>Customize your editor:</strong> Tailor the editor\'s\n\t\tconfiguration to match your application\'s style and requirements. Or\n\t\teven write your plugin!\n\t</li>\n</ol>\n<p>\n\tKeep experimenting, and don\'t hesitate to push the boundaries of what you\n\tcan achieve with CKEditor 5. Your feedback is invaluable to us as we strive\n\tto improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n\t<li>📝 <a href="https://portal.ckeditor.com/checkout?plan=free">Trial sign up</a>,</li>\n\t<li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n\t<li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n\t<li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n\t<li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n\tSee this text, but the editor is not starting up? Check the browser\'s\n\tconsole for clues and guidance. It may be related to an incorrect license\n\tkey if you use premium features or another feature-related requirement. If\n\tyou cannot make it work, file a GitHub issue, and we will help as soon as\n\tpossible!\n</p>\n',
+
 	language: 'fr',
 	licenseKey: LICENSE_KEY,
 	link: {
@@ -251,8 +249,12 @@ const editorConfig = {
 	},
 	table: {
 		contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
-	},
-	translations: [translations]
+	}
 };
 
 
+
+
+
+
+  
