624367e3354ae75cf6c10b0be8da66882e001ee1
Mehdi Boumediene <elm3hdi@gmail.com>
2025-06-30 13:24:12 +0100

    Frontend: Traduction Actualité & Appels

--- src/Controller/FrontendController.php
+++ src/Controller/FrontendController.php
@@ -94,7 +94,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
     }
 
       #[Route('/{_locale}/news/{id}', name: 'app_acctualites_show', methods: ['GET'], requirements: ['_locale' => 'fr|it|en|ar'], defaults: ['_locale' => 'fr'])]
-    public function acctualitesShow(Acctualites $acctualites,Request $request): Response
+    public function acctualitesShow(AcctualitesRepository $AcctualitesRepository,Request $request): Response
     {
         $locale = $request->getLocale(); // récupère la locale courante
 
