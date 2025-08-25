758cca5e6b8689225b7178c6cb5b14766f860870
Mehdi Boumediene <elm3hdi@gmail.com>
2025-08-25 10:18:11 +0100

    Backend:Candidatures-Note

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/note')]
+#[Route('/{_locale}/admin/note')]
 final class NoteController extends AbstractController
 {
     #[Route(name: 'app_note_index', methods: ['GET'])]
