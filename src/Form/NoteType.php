0a0c4e8ab1e559fd6c46cd57974616e6b1a19f5b
Mehdi Boumediene <elm3hdi@gmail.com>
2025-08-25 11:04:21 +0100

    Backend:Candidatures-Note

--- src/Form/NoteType.php
+++ src/Form/NoteType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Candidatures;
 use App\Entity\Note;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,7 +15,18 @@ class NoteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('valeur')
+            ->add('valeur', ChoiceType::class, [
+        'choices'  => [
+            '⭐' => 1,
+            '⭐⭐' => 2,
+            '⭐⭐⭐' => 3,
+            '⭐⭐⭐⭐' => 4,
+            '⭐⭐⭐⭐⭐' => 5,
+        ],
+        'expanded' => true, // radio boutons
+        'multiple' => false,
+        'label' => 'Note'
+    ])
             ->add('candidat', EntityType::class, [
                 'class' => Candidatures::class,
                 'choice_label' => 'id',
