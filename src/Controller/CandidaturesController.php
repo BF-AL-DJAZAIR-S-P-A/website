d14f43066fd881778b134e4051d9b76cef533498
Mehdi Boumediene <elm3hdi@gmail.com>
2025-08-25 15:59:01 +0100

    Backend:Candidatures-Mail

--- src/Controller/CandidaturesController.php
+++ src/Controller/CandidaturesController.php
@@ -101,7 +101,7 @@ $mail->setCandidat($candidature);
 $mail->setEmail($candidature->getEmail() ?? ''); // pré-remplir email candidat si disponible
 $mail->setObjet('Votre candidature chez BF ALDJAZAIR');
 
-$mail->setContenu(htmlspecialchars('<p>Bonjour '.$candidature->getPrenom().'</p>'));
+$mail->setContenu('<p>Bonjour '.$candidature->getPrenom() .'</p>');
 
 $formMail = $this->createForm(MailType::class, $mail);
 $formMail->handleRequest($request);
