1d2f80f5b489f367abec17481f3c4266bcfd2696
Mehdi Boumediene <elm3hdi@gmail.com>
2025-08-27 10:21:56 +0100

    Backend:Candidatures-Mail

--- src/Controller/CandidaturesController.php
+++ src/Controller/CandidaturesController.php
@@ -16,7 +16,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
